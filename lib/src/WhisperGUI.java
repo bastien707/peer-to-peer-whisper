52528e7e9788e9d71e1e4e9b09f6bd9945fbb130
bastien707 <bastien.facqueur@efrei.net>
2023-06-03 22:18:52 +0200

    chore: refactor GUI components

--- lib/src/WhisperGUI.java
+++ lib/src/WhisperGUI.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 
 public class WhisperGUI {
     private Node node;
-    private JFrame frame;
     private JTextArea chatArea;
     private JTextField inputField;
     private JButton sendButton;
@@ -16,57 +15,96 @@ public class WhisperGUI {
     private JButton exitButton;
     private JList<String> nodeList;
     private JComboBox<String> comboBox;
-    private HashMap<String, JTextArea> privateChats; // <name, chatArea>
+    private HashMap<String, JTextArea> privateChats;
     private JScrollPane currentPrivateChatScrollPane;
+    private JPanel privateChatPanel;
 
     public WhisperGUI() throws IOException {
+        initComponents();
+        initListeners();
+        node.connect();
+        node.startListening();
+    }
 
-        //+-------------------------------+
-        //|  Create the frame and connect |
-        //+-------------------------------+
-
+    /**
+     * Initialize the components of the GUI
+     * @throws IOException if the socket is not valid
+     */
+    private void initComponents() throws IOException {
         String name = showUsernameDialog();
         privateChats = new HashMap<>();
         nodeList = new JList<>();
         comboBox = new JComboBox<>();
         currentPrivateChatScrollPane = new JScrollPane();
 
-        frame = new JFrame("Whisper Chat - " + name);
+        JFrame frame = new JFrame("Whisper Chat - " + name);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
 
-        chatArea = new JTextArea();
-        chatArea.setEditable(false);
-        chatArea.append("#Welcome to Whisper Chat, " + name + "!\n");
-        chatArea.append("#Type your message in the input field and press Send to start chatting.\n");
-        chatArea.append("#To see the list of connected nodes, click the Node List button.\n");
-        chatArea.append("#To exit the chat, click the Exit button or close the window.\n\n");
-
-        // Connect to the network after having created the GUI
-        // so that the GUI is ready to receive messages
+        chatArea = createChatArea(name);
         node = new Node(name, this);
-        node.connect();
-        node.startListening();
+        JTabbedPane tabbedPane = createTabbedPane();
+        frame.add(tabbedPane, BorderLayout.CENTER);
+        frame.setVisible(true);
+    }
 
-        //+-----------------------------+
-        //|  GUI Components and Layout  |
-        //+-----------------------------+
+    /**
+     * Create the general chat area with welcome messages
+     * @param name the name of the user
+     * @return the chat area
+     */
+    private JTextArea createChatArea(String name) {
+        JTextArea chat = new JTextArea();
+        chat.setEditable(false);
+        chat.append("#Welcome to Whisper Chat, " + name + "!\n");
+        chat.append("#Type your message in the input field and press Send to start chatting\n");
+        chat.append("#To exit the chat, click the Exit\n\n");
 
-        // Declate the tabbed pane
+        return chat;
+    }
+
+    /**
+     * Create the tabbed pane with the general chat and the private chat
+     * @return the tabbed pane
+     */
+    private JTabbedPane createTabbedPane() {
         JTabbedPane tabbedPane = new JTabbedPane();
         tabbedPane.setSize(800, 100);
 
-        // Declare the panels for the tabbed pane
-        JPanel generalChat = new JPanel(new BorderLayout());
-        JPanel privateChatPanel = new JPanel(new BorderLayout());
+        JPanel generalChat = createGeneralChatPanel();
+        JPanel privateChatPanel = createPrivateChatPanel();
 
-        // Declare the node list right pane for the general chat
+        tabbedPane.add("General", generalChat);
+        tabbedPane.add("Private", privateChatPanel);
+
+        return tabbedPane;
+    }
+
+    private JPanel createGeneralChatPanel() {
+        JPanel generalChat = new JPanel(new BorderLayout());
+
         JScrollPane scrollPane = new JScrollPane(nodeList);
-        Dimension preferredSize = new Dimension(200, 600); // Adjust the width and height as needed
+        Dimension preferredSize = new Dimension(200, 600);
+
+        JPanel bottomPanel = createBottomPanel();
+
+        generalChat.add(scrollPane, BorderLayout.EAST);
+        generalChat.add(new JScrollPane(chatArea), BorderLayout.CENTER);
+        generalChat.add(bottomPanel, BorderLayout.SOUTH);
+
         scrollPane.setPreferredSize(preferredSize);
 
-        // Declare the bottom panel for the general chat
+        return generalChat;
+    }
+
+    /**
+     * Create the bottom panel which contains the input field and the buttons
+     * It is used for general chat
+     * @return the bottom panel
+     */
+    private JPanel createBottomPanel() {
         JPanel bottomPanel = new JPanel(new BorderLayout());
+
         inputField = new JTextField();
         sendButton = new JButton("Send");
         exitButton = new JButton("Exit");
@@ -75,11 +113,16 @@ public class WhisperGUI {
         bottomPanel.add(sendButton, BorderLayout.EAST);
         bottomPanel.add(exitButton, BorderLayout.WEST);
 
-        generalChat.add(scrollPane, BorderLayout.EAST);
-        generalChat.add(new JScrollPane(chatArea), BorderLayout.CENTER);
-        generalChat.add(bottomPanel, BorderLayout.SOUTH);
+        return bottomPanel;
+    }
 
-        // Declare the private chat panel
+    /**
+     * Create the private chat panel which contains the combo box and the input field
+     * Combo box is used to select the recipient of the private message
+     * @return the private chat panel
+     */
+    private JPanel createPrivateChatPanel() {
+        privateChatPanel = new JPanel(new BorderLayout());
         JPanel bottomSendPanel = new JPanel(new BorderLayout());
 
         sendPrivateButton = new JButton("Send");
@@ -91,17 +134,13 @@ public class WhisperGUI {
         privateChatPanel.add(comboBox, BorderLayout.NORTH);
         privateChatPanel.add(bottomSendPanel, BorderLayout.SOUTH);
 
-        tabbedPane.addTab("General", generalChat);
-        tabbedPane.addTab("Private", privateChatPanel);
+        return privateChatPanel;
+    }
 
-        frame.add(tabbedPane, BorderLayout.CENTER);
-
-        frame.setVisible(true);
-
-        //+-------------------------+
-        //|  Listen for user input  |
-        //+-------------------------+
-
+    /**
+     * Initialize all listeners
+     */
+    private void initListeners() {
         exitButton.addActionListener(e -> {
             try {
                 node.disconnect();
@@ -109,35 +148,6 @@ public class WhisperGUI {
                 ex.printStackTrace();
             }
             System.exit(0);
-        });
-
-        sendButton.addActionListener(e -> {
-            String message = inputField.getText();
-            if (!message.isEmpty() && !message.contains(":")) {
-                try {
-                    Message msg = new Message("MULTICAST_MESSAGE", node.getName(), message, null);
-                    Message.broadcast(node.getMulticastSocket(), msg, node.getGroup());
-                    chatArea.append("You: " + message + "\n");
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
-            inputField.setText("");
-        });
-
-        sendPrivateButton.addActionListener(e -> {
-            String message = privateInputField.getText();
-            String recipient = (String) comboBox.getSelectedItem();
-            if (!message.isEmpty() && !message.contains(":")) {
-                try {
-                    Message msg = new Message("PRIVATE_MESSAGE", node.getName(), message, null);
-                    Message.sendMessageObject(node.getSocket(), msg, node.getNodes().get(recipient));
-                    privateChats.get(recipient).append("You: " + message + "\n");
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
-            privateInputField.setText("");
         });
 
         inputField.addKeyListener(new KeyAdapter() {
@@ -149,37 +159,87 @@ public class WhisperGUI {
             }
         });
 
-        /**
-         * Every time the user selects a different recipient from the combo box,
-         * the private chat area is updated to show the messages sent to the selected recipient.
-         */
-        comboBox.addActionListener(e -> {
-            String selectedRecipient = (String) comboBox.getSelectedItem();
-            if (selectedRecipient != null) {
-                if (!privateChats.containsKey(selectedRecipient)) {
-                    // Create a new private chat area
-                    JTextArea privateChatArea = new JTextArea();
-                    privateChatArea.setEditable(false);
-                    privateChats.put(selectedRecipient, privateChatArea);
+        privateInputField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    sendPrivateButton.doClick();
                 }
-
-                // Remove the previous JScrollPane if it exists
-                if (currentPrivateChatScrollPane != null) {
-                    privateChatPanel.remove(currentPrivateChatScrollPane);
-                }
-                // Add the new JScrollPane and update the reference
-                currentPrivateChatScrollPane = new JScrollPane(privateChats.get(selectedRecipient));
-                privateChatPanel.add(currentPrivateChatScrollPane, BorderLayout.CENTER);
-                privateChatPanel.revalidate();
-                privateChatPanel.repaint();
             }
         });
+
+        sendButton.addActionListener(e -> sendGeneralMessage());
+        sendPrivateButton.addActionListener(e -> sendPrivateMessage());
+        comboBox.addActionListener(e -> updatePrivateChat());
     }
 
-    //+-------------------------+
-    //| Functions for the GUI   |
-    //+-------------------------+
+    /**
+     * Broadcast a message to all nodes
+     * Message is sent only if it is not empty and does not contain ":"
+     */
+    private void sendGeneralMessage() {
+        String message = inputField.getText();
+        if (!message.isEmpty() && !message.contains(":")) {
+            try {
+                Message msg = new Message("MULTICAST_MESSAGE", node.getName(), message, null);
+                Message.broadcast(node.getMulticastSocket(), msg, node.getGroup());
+                chatArea.append("You: " + message + "\n");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        inputField.setText("");
+    }
 
+    /**
+     * Send a private message to a specific node
+     * Message is sent only if it is not empty and does not contain ":"
+     */
+    private void sendPrivateMessage() {
+        String message = privateInputField.getText();
+        String recipient = (String) comboBox.getSelectedItem();
+        if (!message.isEmpty() && !message.contains(":")) {
+            try {
+                Message msg = new Message("PRIVATE_MESSAGE", node.getName(), message, null);
+                Message.sendMessageObject(node.getSocket(), msg, node.getNodes().get(recipient));
+                privateChats.get(recipient).append("You: " + message + "\n");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        privateInputField.setText("");
+    }
+
+    /**
+     * Update the private chat area when a new recipient is selected
+     * If the private chat area does not exist, it is created
+     * If the private chat area exists, it is removed and the new one is added with the updated reference
+     * If the selected recipient is null, nothing happens
+     */
+    private void updatePrivateChat() {
+        String selectedRecipient = (String) comboBox.getSelectedItem();
+        if (selectedRecipient != null) {
+            if (!privateChats.containsKey(selectedRecipient)) {
+                // Create a new private chat area
+                JTextArea privateChatArea = new JTextArea();
+                privateChatArea.setEditable(false);
+                privateChats.put(selectedRecipient, privateChatArea);
+            }
+
+            if (currentPrivateChatScrollPane != null) {
+                privateChatPanel.remove(currentPrivateChatScrollPane);
+            }
+
+            currentPrivateChatScrollPane = new JScrollPane(privateChats.get(selectedRecipient));
+            privateChatPanel.add(currentPrivateChatScrollPane, BorderLayout.CENTER);
+            privateChatPanel.revalidate();
+            privateChatPanel.repaint();
+        }
+    }
+
+    /**
+     * Update the list of nodes in the general chat right panel
+     */
     public void updateNodeList() {
         DefaultListModel<String> listModel = new DefaultListModel<>();
         for (String nodeName : node.getNodes().keySet()) {
@@ -188,12 +248,21 @@ public class WhisperGUI {
         nodeList.setModel(listModel);
     }
 
+    /**
+     * Update the combo box with the list of nodes on the private chat panel
+     */
     public void updateComboBox() {
         String[] list = node.getNodes().keySet().toArray(new String[0]);
         DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>(list);
         comboBox.setModel(model);
     }
 
+    /**
+     * Update the private chat area when a new message is received for the selected recipient
+     * If the private chat area does not exist with the sender, it is created
+     * @param message the message received
+     * @param sender the sender of the message
+     */
     public void updatePrivateChatArea(String message, String sender) {
         if (!privateChats.containsKey(sender)) {
             JTextArea privateChat = new JTextArea();
@@ -203,10 +272,18 @@ public class WhisperGUI {
         privateChats.get(sender).append(message + "\n");
     }
 
+    /**
+     * Update the general chat area when a new message is received
+     * @param message the message received
+     */
     public void updateChatArea(String message) {
         chatArea.append(message + "\n");
     }
 
+    /**
+     * Show a dialog to enter the username before starting the chat
+     * @return the username entered
+     */
     private String showUsernameDialog() {
         String username = "";
         JTextField usernameField = new JTextField(20);
